21d268f82eeb76e1c17daae0dfb7d66fb2a14790
Silverprince01 <87282717+Silverprince01@users.noreply.github.com>
2023-10-10 22:59:54 +0100

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import hero from "../assets/images/hero.png";
 const Hero = () => {
   const social_media = [
-    "logo-instagram",
-    "logo-facebook",
-    "logo-linkedin",
-    "logo-twitter",
+    {
+      id: 1,
+      link: "https://www.linkedin.com/in/badru-fuad",
+      logo: "logo-linkedin",
+    },
+    // "logo-facebook",
+    // "",
+    // "logo-twitter",
   ];
   return (
     <section
       id="home"
       className="min-h-screen flex py-10 md:flex-row flex-col justify-center md:justify-start items-center bg-gray-100"
     >
-      
       <div className="px-8">
         <div className="md:text-left text-center">
           <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-gray-800 font-bold">
@@ -29,10 +32,13 @@ const Hero = () => {
           <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
             {social_media?.map((icon) => (
               <div
-                key={icon}
+                key={icon.id}
                 className="text-gray-600 hover:text-white cursor-pointer "
               >
-                <ion-icon name={icon}></ion-icon>
+                <a href={icon.link}>
+
+                <ion-icon name={icon.logo}></ion-icon>
+                </a>
               </div>
             ))}
           </div>
